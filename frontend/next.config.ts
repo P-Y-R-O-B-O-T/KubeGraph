b7e03e81724eea3923fbcc63b25bd8c277ff75c8
kushl <kushl@kushl.com>
2025-06-24 21:47:41 +0530

    frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+const nextConfig = {
+  // You can add other Next.js configurations here, for example:
+  // output: 'standalone',
+
+  // This is the part that solves your problem:
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
